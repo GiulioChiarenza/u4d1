a2b3d43a28329c902d5d65e83d2753434fe2b6ff
GiulioChiarenza <giulio.chiarenza@outlook.it>
2024-03-04 17:35:40 +0100

    add scanner

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 public class Esercizio3 {
     public static void  main(String[] args){
         System.out.println("hello");
-double risultatoPerimetroRettangolo = perimetroRettangolo(5.5, 10.5);
+        Scanner scanner = new Scanner(System.in);
+        double latoR1=Double.parseDouble(scanner.nextLine());
+        double latoR2=Double.parseDouble(scanner.nextLine());
+
+double risultatoPerimetroRettangolo = perimetroRettangolo(latoR1, latoR2);
         System.out.println(risultatoPerimetroRettangolo);
 int numero1 = 4;
 int numero2 = 5;
         System.out.println("il numero " + numero1 + " è " + (pariDispari(numero1)));
         System.out.println("il numero " + numero2 + " è " + (pariDispari(numero2)));
 
-        double lato1= 3.5;
-        double lato2= 4.5;
-        double lato3= 5.5;
+        double lato1= Double.parseDouble(scanner.nextLine());
+        double lato2= Double.parseDouble(scanner.nextLine());
+        double lato3= Double.parseDouble(scanner.nextLine());
         double area= perimetroTriangolo(lato1, lato2, lato3);
         System.out.println("l'area del triangolo è: " + area);
     }
@@ -25,7 +31,7 @@ int numero2 = 5;
     public static double perimetroTriangolo(double num1, double num2, double num3){
         double semiPerimetro= (num1+num2+num3) / 2;
         return Math.sqrt(semiPerimetro*(semiPerimetro-num1)*(semiPerimetro-num2)*(semiPerimetro-num3));
-       
+
     }
 
     }
